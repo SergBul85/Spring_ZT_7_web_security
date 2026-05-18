a3245ec50fc8ea467dcb92423a82ef9837bb162e
SergBul85 <donetsk55555@gmail.com>
2026-05-18 20:09:10 +0300

    Init commit

---
+++ src/main/java/com/hstn/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,20 @@
+package com.hstn.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE").build())
+                .withUser(userBuilder.username("elena").password("elena").roles("HR").build())
+                .withUser(userBuilder.username("ivan").password("ivan").roles("MANGER", "HR").build());
+    }
+}
