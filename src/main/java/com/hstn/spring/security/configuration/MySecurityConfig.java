c5c9751a3efbab4ed42c0f5777a68b7b03f266c7
SergBul85 <donetsk55555@gmail.com>
2026-05-18 21:11:18 +0300

    L_92_authorization

--- src/main/java/com/hstn/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/hstn/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.hstn.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,6 +16,15 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE").build())
                 .withUser(userBuilder.username("elena").password("elena").roles("HR").build())
-                .withUser(userBuilder.username("ivan").password("ivan").roles("MANGER", "HR").build());
+                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR").build());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "MANAGER", "HR")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manger_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
     }
 }
